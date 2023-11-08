ba2181d53debf3c1ba9fc316a15b557d4f84f5c4
Vladimir Zaytsev <ivovochka48@gmial.com>
2023-11-08 15:40:22 +0300

    3 примера для того, чтобы не потерять контекст this

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -27,18 +27,52 @@ import "./App.css";
 //     </div>
 //   );
 // }
+import { Component } from "react";
+class WhoAmI extends Component {
+  constructor(props) {
+    super(props);
+    //внутренне состояние пропса-------
+    this.state = {
+      years: 27,
+      text: "+++",
+      position: "",
+    };
+    //---------------------------
+    this.nextYear = this.nextYear.bind(this)
+  }
 
-function WhoAmI({ name, surname, link }) {
-  return (
-    <div>
-      <h1>
-        {/* {" "}
-        My name is {name.firstName}, surname - {surname} */}
-        My name is {name()}, surname - {surname}
-      </h1>
-      <a href={link}>My profile</a>
-    </div>
-  );
+  nextYear = () => {
+    //изминения состояния setState - для отрисовки
+    this.setState((state) => ({
+      years: state.years + 1,
+    }));
+  };
+
+  commitInputChanges = (e, color) => {
+    console.log(color);
+    this.setState({
+      position: e.target.value,
+    });
+  };
+
+  render() {
+    const { name, surname, link } = this.props;
+    const { position, years } = this.state;
+    return (
+      <div>
+        <button onClick={this.nextYear}>{this.state.text}</button>
+        <h1>
+          My name is {name}, surname - {surname}, age - {years},
+          position - {position}
+        </h1>
+        <a href={link}>My profile</a>
+        <form>
+          <span>Ведите должность</span>
+          <input type="text" onChange={(e) => this.commitInputChanges(e, 'some color')}></input>
+        </form>
+      </div>
+    );
+  }
 }
 
 //Компонент ни в коме случае не должен изменять пропсы - 1 раз создали и его изменить нельзя
@@ -53,14 +87,9 @@ function App() {
         surname="Smith"
         link="facebook.com"
   />*/}
-     {/* <WhoAmI name="Alex" surname="Smooth" link="inst.com" /> */}
-      <WhoAmI
-        name={() => {
-          return "Jameson";
-        }}
-        surname="Smooth"
-        link="inst.com"
-      />
+      {/* <WhoAmI name="Alex" surname="Smooth" link="inst.com" /> */}
+      <WhoAmI name="Jhon" surname="Smith" link="facebook.com" />
+      <WhoAmI name="Alex" surname="Smith" link="facebook.com" />
     </div>
   );
 }
